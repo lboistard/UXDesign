ef559aa86ea8db00f6b9513f80dffcae3090aed1
Lucas Boistard <49691983+lboistard@users.noreply.github.com>
2020-09-28 17:35:31 +0200

    add account managament for patient

---
+++ app/src/main/java/com/example/dmp/ComptePatientActivity.java
@@ -0,0 +1,14 @@
+package com.example.dmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComptePatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compte_patient);
+    }
+}
