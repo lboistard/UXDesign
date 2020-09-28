38b352ce28f99c6b180b5e7c9cfcfdbb0f0f2c5d
Lucas Boistard <49691983+lboistard@users.noreply.github.com>
2020-09-28 18:51:40 +0200

    add compte medecin layout

---
+++ app/src/main/java/com/example/dmp/CompteMedecinActivity.java
@@ -0,0 +1,14 @@
+package com.example.dmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompteMedecinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compte_medecin);
+    }
+}
