11c9bf264eccd944882c9b189a6350d9e4dab783
AliBPRO <59185023+AliBPRO@users.noreply.github.com>
2020-09-30 11:26:26 +0200

    accordion

---
+++ app/src/main/java/com/example/dmp/CompteRenduPatientActivity.java
@@ -0,0 +1,9 @@
+package com.example.dmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.riyagayasen.easyaccordion.AccordionView;
+
+public class CompteRenduPatientActivity extends AppCompatActivity {
+    AccordionView accordionView = new AccordionView(this);
+}
